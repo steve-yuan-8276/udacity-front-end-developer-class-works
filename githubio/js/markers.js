cb36e49fec85be468896f4e5cde3f0aef339bf84
steve-yuan-8276 <yuanf02@gmail.com>
2017-11-24 22:19:45 +0800

    fix bugs:app.js

---
+++ githubio/js/markers.js
@@ -0,0 +1,105 @@
+//定义景点位置信息
+const myLocations = [
+    {
+        title: '天安门（Tiananmen Square）',
+        lat: 39.90872,
+        lng: 116.39748,
+        type: 'establishment'
+    },
+    {
+        title: '故宫（Forbidden City）',
+        lat: 39.916345,
+        lng: 116.397155,
+        type: 'museum'
+    },
+    {
+        title: '天坛（Temple of Heaven）',
+        lat: 39.883704,
+        lng: 116.41284,
+        type: 'historic temple'
+    },
+    {
+        title: '中央电视台（CCTV）',
+        lat: 39.915275,
+        lng: 116.464231,
+        type: 'You know who'
+    },
+    {
+        title: '颐和园（Summer Palace）',
+        lat: 39.999982,
+        lng: 116.275461,
+        type: 'Iconic lakeside retreat for royalty'
+    },
+    {
+        title: '人民大会堂（Great Hall of the People）',
+        lat: 39.905,
+        lng: 116.393942,
+        type: 'government building'
+    },
+    {
+        title: '鸟巢（National Stadium）',
+        lat: 39.992943,
+        lng: 116.396511,
+        type: 'Stadium'
+    },
+    {
+        title: '三里屯（Sanlitun）',
+        lat: 39.932472,
+        lng: 116.455126,
+        type: 'shopping area'
+    },
+    {
+        title: '首都博物馆（Capital Museum）',
+        lat: 39.906434,
+        lng: 116.341941,
+        type: 'museum'
+    },
+    {
+        title: '北京动物园（Beijing Zoo）',
+        lat: 39.938794,
+        lng: 116.339571,
+        type: 'ZOO'
+    },
+    {
+        title: '国家大剧院（National Centre for the Performing Arts）',
+        lat: 39.904799,
+        lng: 116.389671,
+        type: 'Theater'
+    },
+    {
+        title: '国家博物馆（National Museum of China）',
+        lat: 39.905095,
+        lng: 116.401583,
+        type: 'museum'
+    },
+    {
+        title: '王府井（Wangfujing）',
+        lat: 39.916246,
+        lng: 116.411168,
+        type: 'shopping area'
+    },
+    {
+        title: '北海公园（Beihai Park）',
+        lat: 39.925447,
+        lng: 116.389264,
+        type: 'park'
+    },
+    {
+        title: '798艺术区',
+        lat: 39.984128,
+        lng: 116.495707,
+        type: 'Modern galleries'
+    },
+    {
+        title: '后海（Houhai）',
+        lat: 39.944201,
+        lng: 116.381763,
+        type: 'Lively bar, nightlife'
+    },
+    {
+        title: '中国电影博物馆（National Film Museum）',
+        lat: 39.996205,
+        lng: 116.520936,
+        type: 'Modern museum'
+    }
+];
